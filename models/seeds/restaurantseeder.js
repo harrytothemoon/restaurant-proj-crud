49e15dd284d358c8a0f1fafae10f6d9a9bc7502a
Harry <harry811016@gmail.com>
2020-06-07 23:09:18 +0800

    modify todoSeeder

--- models/seeds/restaurantseeder.js
+++ models/seeds/restaurantseeder.js
@@ -1,24 +1,58 @@
-const Restaurant = require('../restaurant')
 const restaurantList = require('./restaurant.json')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const Restaurant = require('../restaurant')
+const User = require('../user')
 
 const db = require('../../config/mongoose')
 
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+  },
+]
+
 db.once('open', () => {
   console.log('mongodb connected!')
-
-  for (let i = 0; i < restaurantList.results.length; i++) {
-    Restaurant.create({
-      name: restaurantList.results[i].name,
-      category: restaurantList.results[i].category,
-      rating: restaurantList.results[i].rating,
-      image: restaurantList.results[i].image,
-      location: restaurantList.results[i].location,
-      google_map: restaurantList.results[i].google_map,
-      phone: restaurantList.results[i].phone,
-      description: restaurantList.results[i].description
-    })
-  }
-
+  userSeed(SEED_USER[0], 0)
+  userSeed(SEED_USER[1], 3)
   console.log('done!')
 })
+
+function userSeed(userTest, dataFrom) {
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(userTest.password, salt))
+    .then(hash => User.create({
+      name: userTest.name,
+      email: userTest.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 3 },
+        (_, i) => Restaurant.create({
+          name: restaurantList.results[i + dataFrom].name,
+          category: restaurantList.results[i + dataFrom].category,
+          rating: restaurantList.results[i + dataFrom].rating,
+          image: restaurantList.results[i + dataFrom].image,
+          location: restaurantList.results[i + dataFrom].location,
+          google_map: restaurantList.results[i + dataFrom].google_map,
+          phone: restaurantList.results[i + dataFrom].phone,
+          description: restaurantList.results[i + dataFrom].description,
+          userId
+        })
+      ))
+    })
+}
 

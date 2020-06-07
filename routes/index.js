c42c51754e1e24f3b62f2bc5d5991d292d82b8e3
Harry <harry811016@gmail.com>
2020-06-07 20:40:27 +0800

    featfeat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const crud = require('./modules/crud')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', crud)
 router.use('/sort', sort)
+router.use('/users', users)
 
 module.exports = router

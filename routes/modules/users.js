da4aa2edf4b5b8f2c9293c5a747628837dcc5332
Harry <harry811016@gmail.com>
2020-06-07 20:42:19 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,4 +3,11 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+// routes/modules/users.js
+router.post('/login', (req, res) => {
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 module.exports = router
